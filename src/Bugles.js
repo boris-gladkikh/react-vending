5b77fcda034aa3c5256d48ecde97e71e6b17cc4e
gladkill <gladkillmusic@gmail.com>
2020-04-27 12:51:41 -0700

    added bugle functionality

--- src/Bugles.js
+++ src/Bugles.js
@@ -1,13 +1,22 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { Link } from "react-router-dom";
 import "./Bugles.css"
 
 function Bugles(){
+  const [death, setDeath] = useState("Hidden");
+  const [bugleView, setBugleView] = useState("");
+
+  function handleBugle(){
+    setDeath("");
+    setBugleView("Hidden");
+  }
   return(
     <div className="bugleBox">
       <h3>Here are your bugles, with a side of mystery sauce.</h3>
-      <img className="buglesImg" src="https://3.bp.blogspot.com/_5kM8AWE0GaI/TNIzjcJ14pI/AAAAAAAADwk/OYCNgIrkWs0/s1600/photo%2819%29.JPG" alt="bugles, dawg"></img>
-      <h5>May God have mercy on us all.</h5>
+      <img className={`buglesImg ${bugleView}`} src="https://3.bp.blogspot.com/_5kM8AWE0GaI/TNIzjcJ14pI/AAAAAAAADwk/OYCNgIrkWs0/s1600/photo%2819%29.JPG" alt="bugles, dawg"></img>
+      <img className={`deathImg ${death}`} src="https://i.kym-cdn.com/entries/icons/facebook/000/029/198/Dark_Souls_You_Died_Screen_-_Completely_Black_Screen_0-2_screenshot.jpg" alt="You Died."></img><br/>
+      <h5 className={bugleView}>May God have mercy on us all.</h5>
+      <button onClick={handleBugle}>Eat Bugles!</button><br/>
       <Link to="/">Go Back</Link>
 
     </div>
