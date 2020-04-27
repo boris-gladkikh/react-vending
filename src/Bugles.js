9dbde0cb0c7cc72901276964be871a03b17533f6
gladkill <gladkillmusic@gmail.com>
2020-04-27 12:55:02 -0700

    bugles functionality tweaks

--- src/Bugles.js
+++ src/Bugles.js
@@ -15,7 +15,7 @@ function Bugles(){
       <h3>Here are your bugles, with a side of mystery sauce.</h3>
       <img className={`buglesImg ${bugleView}`} src="https://3.bp.blogspot.com/_5kM8AWE0GaI/TNIzjcJ14pI/AAAAAAAADwk/OYCNgIrkWs0/s1600/photo%2819%29.JPG" alt="bugles, dawg"></img>
       <img className={`deathImg ${death}`} src="https://i.kym-cdn.com/entries/icons/facebook/000/029/198/Dark_Souls_You_Died_Screen_-_Completely_Black_Screen_0-2_screenshot.jpg" alt="You Died."></img><br/>
-      <h5 className={bugleView}>May God have mercy on us all.</h5>
+      <h5 className={bugleView}>Abandon Hope, Ye Who Ingest This.</h5>
       <button onClick={handleBugle}>Eat Bugles!</button><br/>
       <Link to="/">Go Back</Link>
 
